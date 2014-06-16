91719eba5e0308c11bc546338f8f939089b6c176
William Riley-Land <william@kun.io>
2014-06-16 14:12:37 -0500

    v0.4.0 Add support for generators and support for additional template engines.

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
+
+// # Wainwright
+// ## Dependencies
 var fs = require('fs');
 var path = require('path');
 var deco = require('deco');
 var es = require('event-stream');
 var yaml = require('js-yaml');
-var hogan = require('hogan.js');
-var xmlb = require('xmlb');
+var mime = require('mime');
 var markdown = require('marked');
-var higlight = require('highlight.js');
-//
+var gulpmatch = require('gulp-match');
+var highlight = require('highlight.js');
+var consolidate = require('consolidate');
+var File = require('vinyl');
+// ## Configuration
+// Set up code highlighting for markdown transforms.
 markdown.setOptions({
   highlight: function (code) {
-    return higlight.highlightAuto(code).value;
+    return highlight.highlightAuto(code).value;
   }
 });
+// ## Private Methods
 // From the wintersmith markdown plugin.
 function parseMetadata (source, callback) {
   var i;
   var lines;
   var markerPad = '';
 
+  if (!source) return callback(null, {});
   if (source.length === 0) return callback(null, {});
 
   try {
@@ -69,101 +77,125 @@ function extractMetadata (content, callback) {
     });
   });
 };
-
-function applyTemplate (templatePath, metadata, callback) {
-  // load template
-  fs.readFile(templatePath, function (error, templateContents) {
-    if (error) return callback(error);
-    var extension = templatePath.split('.').pop();
-    var applied;
-    // get tempalte engine based on extension & apply
-    if (extension === 'mustache') {
-      applied = hogan.compile(templateContents.toString()).render(metadata);
-    }
-    else if (extension === 'xmlb') {
-      applied = xmlb.compile(templateContents.toString())(metadata);
-    }
-    else {
-      return callback(new Error('Unknown template extension.'));
-    }
-
-    callback(null, applied);
-  });
-}
-
-function matchExtension (file) { // TODO use mime module?
-  var extensions = Array.prototype.slice.call(arguments, 1);
-  var match = false;
-  extensions.forEach(function (extension) {
-    if (file.path.indexOf(extension) === file.path.length - extension.length) {
-      match = true;
-    }
-  });
-  return match;
-}
-
+// ## Module Definition
 var wainwright = module.exports = function (options) {
   // Merge instance metadata on top of defaults.
   var defaults = { templateDirectory: './templates' };
   var metadata = deco.merge(defaults, options.metadata);
+  // A stream to emit the wainwright context for each file.
+  var metadataOut = es.through();
   // Build the stream that does all the work.
-  return es.map(function (file, callback) {
-    var parsed;
-    var pathParts;
-    var contents = file.contents ? file.contents.toString() : undefined;
-    // Function to process a file.
-    function applyLocals (locals, callback) {
-      // Rename the file if a new filename was specified.
-      if (locals.filename) {
-        file.path = path.resolve(file.base, locals.filename);
+  var filesOut = es.pipeline(
+    wainwright.context(metadata),
+    wainwright.extract(),
+    wainwright.template(),
+    metadataOut,
+    wainwright.file()
+  );
+  // Get the stream of internal data for use by generators.
+  // Takes the same arguments as `gulp.src`.
+  filesOut.metadata = function (condition) {
+    condition = condition || true;
+    return metadataOut.pipe(es.through(
+      function (metadata) {
+        var c = path.resolve(metadata.cwd, metadata.base, condition);
+        if (!gulpmatch(metadata, c)) return;
+        this.emit('data', metadata);
+      }, 
+      function () { 
+        this.emit('end') 
       }
-      // Pass through files without a template unchanged.
-      if (!locals.template) return callback(null, file);
-      // Otherwise, apply the template.  But first, figure out
-      // where they are located.
-      var templatePath = path.resolve(
-        process.cwd(), 
-        locals.templateDirectory, 
-        locals.template
-      );
-      applyTemplate(templatePath, locals, function (error, applied) {
-        if (error) return callback(error);
-        file.contents = new Buffer(applied);
-        callback(null, file);
-      });
-    }
-    // Pass through directories and empty files.
-    if (!contents) return callback(null, file);
-    // JSON files can't have YAML headers.
-    if (matchExtension(file, '.json')) {
+    ));
+  };
+
+  return filesOut;
+};
+// ## Public Methods
+// Create initial context from file.
+wainwright.context = function (metadata) {
+  return es.map(function (file, callback) {
+    var body = file.contents ? file.contents.toString() : null;
+    callback(null, deco.merge(metadata, {
+      cwd: file.cwd,
+      base: file.base,
+      path: file.path,
+      body: body
+    }));
+  });
+};
+// Extract the metadata from the file header, if applicable.
+wainwright.extract = function () {
+  return es.map(function (metadata, callback) {
+    // Skip empty files and directories.
+    if (!metadata.body) return callback(null, metadata);
+    // Handle JSON files specially.  They aren't allowed to
+    // have YAML headers.
+    if (mime.lookup(metadata.path) === 'application/json') {
       try {
-        parsed = deco.merge(metadata, JSON.parse(contents));
+        metadata = deco.merge(metadata, JSON.parse(metadata.body));
       }
       catch (exception) {
         callback(exception);
         return;
       }
-      applyLocals(parsed, callback);
+      // Ensure metadata path is absolute.
+      metadata.path = path.resolve(metadata.cwd, metadata.base, metadata.path);
+      callback(null, metadata);
       return;
     }
-    // For other files, split out the YAML header and the actual body/content.
-    extractMetadata(contents, function (error, extracted) {
+    // Other files are allowed to specify YAML headers.
+    extractMetadata(metadata.body, function (error, extracted) {
       if (error) return callback(error);
       // Merge file metadata over top of instance metadata.
-      var locals = deco.merge(metadata, extracted.metadata);
-      // Add the file body to the locals.
-      locals.body = extracted.body;
+      metadata = deco.merge(metadata, extracted.metadata);
+      // Add the file body to the metadata.
+      metadata.body = extracted.body;
       // Transform markdown to HTML.
-      if (matchExtension(file, '.md', '.markdown')) {
-        locals.body = markdown(locals.body);
-        if (!locals.filename) {
-          pathParts = file.path.split('.');
-          pathParts.pop();
-          pathParts.push('html');
-          locals.filename = pathParts.join('.');
-        }
+      if (mime.lookup(metadata.path) === 'text/x-markdown') {
+        metadata.body = markdown(metadata.body);
+        pathParts = metadata.path.split('.');
+        pathParts.pop();
+        pathParts.push('html');
+        metadata.path = pathParts.join('.');
       }
-      applyLocals(locals, callback);
+      callback(null, metadata);
     });
   });
+};    
+// Apply template if applicable.
+wainwright.template = function () {
+  return es.map(function (metadata, callback) {
+    // Skip files without template set.
+    if (!metadata.template) return callback(null, metadata);
+    // First, figure out where the templates are located.
+    metadata.template = path.resolve(
+      metadata.cwd, 
+      metadata.templateDirectory, 
+      metadata.template
+    );
+    // Choose which template engine to use by matching the
+    // file extenstion of the template.
+    var extension = metadata.template.split('.').pop();  
+    consolidate[extension](metadata.template, metadata, function (error, applied) {
+      if (error) return callback(error);
+      metadata.rendered = applied;
+      // Send the updated file back out e.g. to `gulp.dest`.
+      callback(null, metadata);
+    });
+  });
+};
+// Convert the wainwright context back into a file.
+wainwright.file = function () {
+  return es.map(function (metadata, callback) {
+    var contents = metadata.rendered || metadata.body;
+    if (!contents) contents = null;
+    else contents = new Buffer(contents);
+
+    callback(null, new File({
+      cwd: metadata.cwd,
+      base: metadata.base,
+      path: metadata.path,
+      contents: contents
+    }));
+  })
 };
